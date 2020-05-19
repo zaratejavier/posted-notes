3f6032d31ea867d97dfbf0ae8a07177a4dd9589b
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-18 21:39:05 -0600

    got create note to work

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,37 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if(@note.update(notes_params))
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  private
+  def note_params
+    params.require(:note.permit(:title,:body))
+  end
+end
